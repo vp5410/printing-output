24240338c075800ff5c1d1dbd96add5816df1f88
vp5410 <156090684+vp5410@users.noreply.github.com>
2024-01-23 22:34:58 -0500

    JAVASCRIPT file added

---
+++ index.js
@@ -0,0 +1,41 @@
+//Task One Begins
+
+let a = 3;
+let b = 5;
+let c;
+let output = `
+let a = 3;
+let b = 5;
+let c;
+------------
+a + b = ${a + b}
+a - b = ${a - b}
+a * b = ${a * b}
+a / b = ${a / b}
+a % b = ${a % b}
+a += b = ${(a += b)}
+a -= b = ${a - +b}
+a *= b = ${(a *= b)}
+a /= b = ${(a /= b)}
+a %= b = ${(a %= b)}
+a == b = ${a == b}
+a != b = ${a != b}
+a > b = ${a > b}
+a < b = ${a < b}
+!a && !c = ${!a && !c}
+!a || !c = ${!a || !c}
+`;
+
+console.log(output);
+
+//Task One Ends
+
+//Task Two Begins
+
+let fname = "Vishnu";
+let lname = "Prasad";
+let email = "vishnup5410@gmail.com";
+let sentence = `My name is ${fname} ${lname}. You can contact me at ${email}.`;
+console.log(sentence);
+
+//Task Two Ends/
